56577a707a3186ff8e73dc33f6e2a4d7e4e4edc4
gusti amanda <gustijayaamanda@gmail.com>
2016-09-20 00:45:35 +0700

    first commit yeah

---
+++ src/main/java/Jobs.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gustiamanda on 9/19/16.
+ */
+public class Jobs {
+    public static void main(String[] args){
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
