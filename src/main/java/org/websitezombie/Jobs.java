d0e62c8710476eeb3899bad80ffe92eefcccf8cc
gusti amanda <gustijayaamanda@gmail.com>
2016-09-21 23:08:47 +0700

    update add jcommander to gradle and implement

--- src/main/java/org/websitezombie/Jobs.java
+++ src/main/java/org/websitezombie/Jobs.java
@@ -1,11 +1,25 @@
 package org.websitezombie;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 /**
  * Created by gustiamanda on 9/19/16.
  */
 public class Jobs {
+    @Parameter(names={"--run", "-r"})
+    String run;
+    @Parameter(names={"--every", "-t"})
+    String every;
     public static void main(String[] args){
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
+        Jobs jobs = new Jobs();
+        new JCommander(jobs, args);
+        jobs.run();
+
+    }
+
+    public void run(){
+        System.out.println(run);
+        System.out.println(every);
     }
 }
